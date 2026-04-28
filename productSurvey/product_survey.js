466f74b56ed425faf49e870d7b4d36a2ce4b2c21
nmic0025-source <nmic0025@gmail.com>
2026-04-28 16:49:02 -0400

    Added userexperience

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -8,6 +8,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userexp = document.getElementById('userExperience').value;
 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -16,6 +17,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('ExpOutput').innerHTML = userexp;
 
     document.getElementById('userInfo').style.display = 'block';
 
